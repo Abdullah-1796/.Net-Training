75d6bc3fde62fe69037c2236e5f3f965dd50ab41
Abdullah Zahid <abdullahzahid1796@gmail.com>
2025-08-20 11:58:59 +0500

    Program.cs changed to previous version

--- EF Core/Program.cs
+++ EF Core/Program.cs
@@ -1,3 +1,7 @@
+using EF_Core.Repositories;
+using EF_Core.Repositories.Interfaces;
+using EF_Core.Services;
+using EF_Core.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using YourNamespace.Data;
 
@@ -9,6 +13,20 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register services
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IRoomService, RoomService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
+
+// Register repositories
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
